c98fbac08cfad7b1cfbff55f0a99dad3af2ad1f9
ogawa <ocmikanmgmg@gmail.com>
2024-08-09 06:23:53 +0900

    a

--- c/atcorder/c17.c
+++ c/atcorder/c17.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 
 int main(void){
-    int i,n,a[101],b[101];
+    int i,n,count=0,a[101],b[101];
     scanf("%d",&n);
+    for(i=0;i<101;i++){
+        b[i]=0;
+        a[i]=0;
+    }
     for(i=0;i<n;i++){
         scanf("%d",&a[i]);
-        b[i]=0;
     }
-    for(i=0;i<n;i++){
+    for(i=0;i<100;i++){
         b[a[i]]=a[i];
-        printf("%d",b[i]);
+        printf("%d\n",b[i]);
     }
+    for(i=0;i<100;i++){
+        if(b[i]!=0){
+            count++;
+            printf("count++\n");
+        }
+    }
+    printf("%d",count);
 
     return 0;
 }
